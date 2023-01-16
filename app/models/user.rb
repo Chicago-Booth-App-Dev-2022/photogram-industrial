0a1b07ab38198f5d7538a98971b1a7f83e20be1c
mparthun <mparthun@chicagobooth.edu>
2023-01-16 01:04:28 +0000

    0115_1904

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
-  has_many :comments, foreign_key: "author_id"
-  has_many :sent_requests, class_name: "FollowRequest", foreign_key: "sender_id"
-  has_many :received_requests, class_name: "FollowRequest", foreign_key: "recipient_id" 
-  has_many :likes, foreign_key: "fan_id"
+  has_many :own_photos, class_name: "Photo", foreign_key: :owner_id
+  has_many :comments, foreign_key: :author_id
+  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: :sender_id
+  has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: :recipient_id 
+  has_many :likes, foreign_key: :fan_id
 end
