1c70523918fa4eb40afaca7dbc0da579ddf5f46f
mparthun <mparthun@chicagobooth.edu>
2023-01-17 17:05:28 +0000

    added following and followers

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,12 @@ Rails.application.routes.draw do
   resources :photos
   resources :users, only: :show
 
-  get ":username/liked" => "photos#liked", as: :liked_photos 
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  #get ":username/feed" => "photos#liked", as: :liked_photos
+  get ":username/followers" => "users#followers", as: :followers # list group
+  get ":username/leaders" => "users#leaders", as: :leaders # list group
 
-  get "/:username" => "users#show"
+  get ":username" => "users#show"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
