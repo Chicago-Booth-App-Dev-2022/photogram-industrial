8adf42819301d08495bb64160a64f3d8f35d380e
mparthun <mparthun@chicagobooth.edu>
2023-01-17 03:23:06 +0000

    Finished video 4

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  get "/users/:id" => "users#show", as: :user
+  
   
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
   resources :users, only: :show
+
+  get "/:username" => "users#show"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
